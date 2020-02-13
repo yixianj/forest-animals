edfa1249071ef4353f7227a5f7f00485c9305748
yixianj <yixianj@umich.edu>
2020-02-13 12:34:53 -0500

    Update config.php

--- config/config.php
+++ config/config.php
@@ -3,8 +3,8 @@
 
     return array(
 	    'host' => 'localhost',
-	    'db_username' => 'root',
-	    'db_password' => '123456',
+	    'db_username' => 'root', // CHANGE ME
+	    'db_password' => '123456', // CHANGE ME
 	    'db_name' => 'forestanimals',
 	    'app_root' => '/forestanimals/',
 	    'images_location' => 'images/',
